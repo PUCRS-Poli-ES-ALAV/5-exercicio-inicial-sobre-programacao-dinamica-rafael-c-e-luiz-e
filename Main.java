b951874d34f15df9244e095af798770dd39a3342
RafaelSCardoso <rafaelcardoso2604@gmail.coim>
2020-09-02 18:38:39 -0300

    2/3 fibo feitos

--- Main.java
+++ Main.java
@@ -6,4 +6,30 @@ public class Main {
     public static void main(String[] args) {
         
     }
+
+    // EX - 1 --------------------------------
+    public static int Fibo_rec(int n) {
+        int a;
+        int b;
+        if (n <= 1)
+            return n;
+        else{
+            a = Fibo_rec(n-1);
+            b = Fibo_rec(n-2);
+        }
+        return (a + b);
+    }
+    // EX - 2 --------------------------------
+    public static int Fibo(int n) {
+        int[] f = {0,1};
+        for (int i = 2; i < f.length; i++) {
+            f[i] = f[i-1] - f[i-2];
+        }
+        return f[n];
+    }
+
+    // EX - 3 --------------------------------
+
+    
+
 }
