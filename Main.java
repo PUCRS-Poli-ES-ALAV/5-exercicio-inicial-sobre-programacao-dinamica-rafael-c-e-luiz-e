832c0a294d784f838fda2bfda4242101c5af7e1a
Luiz Eduardo <luizdudumr@gmail.com>
2020-09-02 20:39:47 -0300

    Adição dos algoritmos Memoized e LookUp e testes para os inteiros

--- Main.java
+++ Main.java
@@ -4,10 +4,30 @@
 public class Main {
 
     public static void main(String[] args) {
+        int a[] = new int[5];
+        int b[] = new int[9];
+        int c[] = new int[17];
+        int d[] = new int[33];
         
+        Fibo_rec(4);
+        Fibo_rec(8);
+        Fibo_rec(16);
+        Fibo_rec(32);
+
+        Fibo(4);
+        Fibo(8);
+        Fibo(16);
+        Fibo(32);
+
+        MemoizedFibo(a, 4);
+        MemoizedFibo(b, 8);
+        MemoizedFibo(c, 16);
+        MemoizedFibo(d, 32);
+
+
     }
 
-    // 1 --------------------------------
+    // 1 ----------------------------------------------------------------
     public static int Fibo_rec(int n) {
         int a;
         int b;
@@ -19,7 +39,7 @@ public class Main {
         }
         return (a + b);
     }
-    // 2 --------------------------------
+    // 2 ----------------------------------------------------------------
     public static int Fibo(int n) {
         int[] f = {0,1};
         for (int i = 2; i < f.length; i++) {
@@ -28,8 +48,24 @@ public class Main {
         return f[n];
     }
 
-    // 3 --------------------------------
+    // 3 ----------------------------------------------------------------
+    public static int MemoizedFibo (int f[], int n){
+        for(int i=0; i<n; i++){
+            f[i] = -1;
+        }
+        return LookUpFibo(f, n);
+    }
 
-
-
+    public static int LookUpFibo(int f[], int n){
+        if(f[n] >= 0){
+            return f[n];
+        }
+        if (n<=1){
+            f[n] = n;
+        }
+        else{
+            f[n] = LookUpFibo(f, n-1) + LookUpFibo(f, n-2);
+        }
+        return f[n];
+    }
 }
