9888fd8406c725661ef1b773064b8c32b2da5e4c
Luiz Eduardo <luizdudumr@gmail.com>
2020-09-03 16:38:12 -0300

    Correção e adição de vetores, melhoria de print e correção do método Fibo

--- Main.java
+++ Main.java
@@ -8,21 +8,30 @@ public class Main {
         int b[] = new int[9];
         int c[] = new int[17];
         int d[] = new int[33];
+        int e[] = new int[129];
+        int f[] = new int[1001];
+        int g[] = new int[10001];
+
+
     
-        System.out.println(Fibo_rec(4));
-        System.out.println(Fibo_rec(8));
-        System.out.println(Fibo_rec(16));
-        System.out.println(Fibo_rec(32));
+        System.out.println("\nFibo rec 4 -------------- " +Fibo_rec(4));
+        System.out.println("Fibo rec 8-------------- " + Fibo_rec(8));
+        System.out.println("Fibo rec 16-------------- " + Fibo_rec(16));
+        System.out.println("Fibo rec 32 -------------- " + Fibo_rec(32) + "\n");
 
-        System.out.println(Fibo(4));    
-        System.out.println(Fibo(8));
-        System.out.println(Fibo(16));
-        System.out.println(Fibo(32));
+        System.out.println("Fibo 4 -------------- " + Fibo(a, 4));    
+        System.out.println("Fibo 8 -------------- " + Fibo(b, 8));
+        System.out.println("Fibo 16 -------------- " + Fibo(c, 16));
+        System.out.println("Fibo 32 -------------- " + Fibo(d, 32) + "\n");
 
-        System.out.println(MemoizedFibo(a, 4));
-        System.out.println(MemoizedFibo(b, 8));
-        System.out.println(MemoizedFibo(c, 16));
-        System.out.println(MemoizedFibo(d, 32));
+        System.out.println("Memoized Fibo 4 -------------- " + MemoizedFibo(a, 4));
+        System.out.println("Memoized Fibo 8 -------------- " + MemoizedFibo(b, 8));
+        System.out.println("Memoized Fibo 16 -------------- " + MemoizedFibo(c, 16));
+        System.out.println("Memoized Fibo 32 -------------- " + MemoizedFibo(d, 32));
+        System.out.println("Memoized Fibo 128 -------------- " + MemoizedFibo(e, 128));
+        System.out.println("Memoized Fibo 1000 -------------- " + MemoizedFibo(f, 1000));
+        System.out.println("Memoized Fibo 10.000 -------------- " + MemoizedFibo(g, 10000) + "\n");
+
 
 
     }
@@ -40,9 +49,10 @@ public class Main {
         return (a + b);
     }
     // 2 ----------------------------------------------------------------
-    public static int Fibo(int n) {
-        int[] f = {0,1};
-        for (int i = 2; i < f.length; i++) {
+    public static int Fibo(int f[], int n) {
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i < n; i++) {
             f[i] = f[i-1] - f[i-2];
         }
         return f[n];
